475fbb887e75e226c21ecd77f081cfc74ee008a3
Zenith Zack <faldund@outlook.com>
2022-01-29 15:32:23 -0500

    Hamburger component

--- src/components/navbar/Hamburger.js
+++ src/components/navbar/Hamburger.js
@@ -2,8 +2,32 @@ import React from 'react';
 
 function Hamburger() {
     return(
-        <div>
-            
+        <div className='hamburger'>
+            <div className='burger burger1'></div>
+            <div className='burger burger2'></div>
+            <div className='burger burger3'></div>
+
+            <style>
+            {`
+                .hamburger {
+                    width: 2rem;
+                    height: 2rem;
+                    display: flex;
+                    justify-content: space-around;
+                    flex-flow: column nowrap;
+                    z-index: 10;
+                }
+
+                .burger {
+                    width: 2rem;
+                    height: 0.25rem;
+                    border-radius: 10px;
+                    background-color: black;
+                    transform-origin: 1px;
+                    transition: all 0.3s linear;
+                }
+            `}
+            </style>
         </div>
     );   
 }
