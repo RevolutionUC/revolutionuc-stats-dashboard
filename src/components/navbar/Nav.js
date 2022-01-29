1df95cbceef8a04df0409b4aad018eaa9df92318
Zenith Zack <faldund@outlook.com>
2022-01-29 15:17:13 -0500

    added labels for accordio

---
+++ src/components/navbar/Nav.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Nav() {
+    return (
+        <div>
+            <div className='navigation'>
+                <ul>
+                    <li>Live</li>
+                    <li>Yearly</li>
+                    <li>Trends</li>
+                </ul>
+            </div>
+        </div>
+    );
+}
+
+export default Nav;
