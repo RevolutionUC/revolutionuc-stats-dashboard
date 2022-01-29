8376b270bf9f9d0c7b4d040a10019b1a05ae2290
Zenith Zack <faldund@outlook.com>
2022-01-29 15:27:06 -0500

    list of items for hamburger

--- src/components/navbar/Nav.js
+++ src/components/navbar/Nav.js
@@ -3,13 +3,40 @@ import React from 'react';
 function Nav() {
     return (
         <div>
-            <div className='navigation'>
+            <div className='navigation' style={{paddingLeft: 400}}>
                 <ul>
                     <li>Live</li>
                     <li>Yearly</li>
                     <li>Trends</li>
                 </ul>
             </div>
+
+            <style jsx>
+            {`
+                .navigation {
+                    width: 100%,
+                    height: 50px;
+                    background-color: blue;
+                }
+                
+                .navigation ul {
+                    display: flex;
+                    flex-wrap: wrap;
+                    float: right;
+                    margin: 20 0px;
+                    padding: 0 25px;
+                    background-color: blue;
+                }
+
+                .navigation ul li {
+                    list-style-type: none;
+                    padding-right: 10px;
+                }
+            `}
+            </style>
+
+
+
         </div>
     );
 }
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import NavbarLogo from '../navbar/NavbarLogo';
 import NavbarNav from './NavbarNav';
+import Nav from './Nav';
 
 const Navbar = function () {
   return (
     <div style={{ position: `fixed`, zIndex: `99` }}>
       <NavbarLogo></NavbarLogo>
       <NavbarNav></NavbarNav>
+      <Nav></Nav>
     </div>
   );
 };
